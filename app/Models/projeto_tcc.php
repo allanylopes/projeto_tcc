6e9bd77f4567854248c4e69e57a944c7c9ffc100
allanylopes <allany.apparecida@etec.sp.gov.br>
2024-09-30 22:46:57 -0300

    first commit

---
+++ app/Models/projeto_tcc.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class projeto_tcc extends Model
+{
+    use HasFactory;
+}
